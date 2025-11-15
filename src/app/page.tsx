976a91db22535924a9a0ed0ed57a92330d02f725
Nirush4 <nirushvj@gmail.com>
2025-11-15 22:45:34 +0100

    feat: :art: feat(about): update section with skills list
    update About section by adding comprehensive skills list and new animation for improved visual experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,13 @@ import { About } from '../components/About';
 // import { Projects } from '../components/Projects';
 // import { ContactForm } from '../components/ContactForm';
 import { Footer } from '../components/Footer';
-import { Background } from '../components/Background';
+// import { Background } from '../components/Background';
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <main className='relative'>
-        <Background />
         <section id='home' className='flex items-center min-h-screen'>
           <div className='container px-6 mx-auto mt-20 sm:mt-0 md:px-8'>
             <Hero />
